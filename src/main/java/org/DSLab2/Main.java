26853b8ed90dd27fe5a162f53236e068525fde66
AhmedAly01 <96546133+AhmedAly01@users.noreply.github.com>
2023-03-16 15:18:45 +0200

    first
    Added samole main and test classes to show you where the Unit tests will be

---
+++ src/main/java/org/DSLab2/Main.java
@@ -0,0 +1,11 @@
+package org.DSLab2;
+
+public class Main {
+
+    public static String helloWorld() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) {
+        System.out.println(helloWorld());
+    }
+}
---
+++ src/test/java/org/DSLab2/JUnitTest.java
@@ -0,0 +1,14 @@
+package org.DSLab2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.DSLab2.Main.helloWorld;
+
+public class JUnitTest {
+    @Test
+    public void testHelloWorld() {
+        String res = "Hello World!";
+        Assertions.assertEquals(res, helloWorld());
+    }
+}
